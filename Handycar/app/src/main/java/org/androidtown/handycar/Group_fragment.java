2bbbb156b89e4eb247763ea50fba13a3696169f4
LeByHo <dlqudgh0731@naver.com>
2017-08-01 17:38:35 +0900

    17-08-01
    add

---
+++ Handycar/app/src/main/java/org/androidtown/handycar/Group_fragment.java
@@ -0,0 +1,110 @@
+package org.androidtown.handycar;
+
+import android.app.Fragment;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
+/**
+ * Created by LEE on 2017-08-01.
+ */
+
+public class Group_fragment extends Fragment{
+    BarChart barChart;
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.group_fragment,container,false);
+        /*barChart = (BarChart)rootView.findViewById(R.id.chart);
+        barChart.zoom(2,1,50,50);
+        barChart.setDescription("");
+        barChart.getAxisLeft(). setLabelCount(10,true);
+        barChart.getAxisRight().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawLabels(false);
+        barChart.getAxisRight().setDrawAxisLine(false);*/
+         HorizontalBarChart barChart= (HorizontalBarChart)rootView.findViewById(R.id.chart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(4f, 0));
+        entries.add(new BarEntry(8f, 1));
+        entries.add(new BarEntry(6f, 2));
+        entries.add(new BarEntry(12f, 3));
+        entries.add(new BarEntry(18f, 4));
+        entries.add(new BarEntry(9f, 5));
+
+        BarDataSet dataset = new BarDataSet(entries, "Score");
+
+      ArrayList<String> labels = new ArrayList<String>();
+        labels.add("January");
+        labels.add("February");
+        labels.add("March");
+        labels.add("April");
+        labels.add("May");
+        labels.add("June");
+        labels.add("May");
+        labels.add("June");
+        labels.add("May");
+        labels.add("June");
+/*
+        ArrayList<BarEntry> group1 = new ArrayList<>();
+        group1.add(new BarEntry(4f, 0));
+        group1.add(new BarEntry(8f, 1));
+        group1.add(new BarEntry(6f, 2));
+        group1.add(new BarEntry(12f, 3));
+        group1.add(new BarEntry(7f, 4));
+        group1.add(new BarEntry(9f, 5));
+        ArrayList<BarEntry> group2 = new ArrayList<>();
+        group2.add(new BarEntry(6f, 0));
+        group2.add(new BarEntry(7f, 1));
+        group2.add(new BarEntry(8f, 2));
+        group2.add(new BarEntry(12f, 3));
+        group2.add(new BarEntry(15f, 4));
+        group2.add(new BarEntry(10f, 5));
+
+        *//*ArrayList<BarEntry> group3 = new ArrayList<>();
+        group3.add(new BarEntry(1f, 0));
+        group3.add(new BarEntry(2f, 1));
+        group3.add(new BarEntry(3f, 2));
+        group3.add(new BarEntry(4f, 3));
+        group3.add(new BarEntry(2f, 4));
+        group3.add(new BarEntry(3f, 5));*//*
+
+        final BarDataSet barDataSet1 = new BarDataSet(group1, "소나타");
+        barDataSet1.setColor(Color.rgb(032, 178, 170));
+
+        barDataSet1.setDrawValues(false);
+        //barDataSet1.setColors(ColorTemplate.COLORFUL_COLORS);
+
+        BarDataSet barDataSet2 = new BarDataSet(group2, "그랜져");
+        barDataSet2.setColor(Color.rgb(0, 51, 102));
+        barDataSet2.setDrawValues(false);
+        //barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
+
+    *//*    BarDataSet barDataSet3 = new BarDataSet(group3, "유지");
+        barDataSet3.setDrawValues(false);
+        //barDataSet3.setColors(ColorTemplate.COLORFUL_COLORS);
+*//*
+        ArrayList<BarDataSet> dataset = new ArrayList<>();
+        dataset.add(barDataSet1);
+        dataset.add(barDataSet2);
+        // dataset.add(barDataSet3);*//*
+*/
+
+        BarData data = new BarData(labels, dataset);
+        // dataset.setColors(ColorTemplate.COLORFUL_COLORS); //
+        barChart.setData(data);
+        barChart.animateY(3000);
+
+
+        return rootView;
+    }
+}
--- Handycar/app/src/main/java/org/androidtown/handycar/Group_total.java
+++ Handycar/app/src/main/java/org/androidtown/handycar/Group_total.java
@@ -1,15 +1,32 @@
 package org.androidtown.handycar;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 /**
  * Created by GE62 on 2017-07-28.
  */
 
 public class Group_total extends AppCompatActivity {
+    FragmentManager fm;
+    Group_fragment Frag;
+    Group_fragment2 Frag2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.group);
+        setup();
+        fm = getFragmentManager();
+        FragmentTransaction tr = fm.beginTransaction();
+        tr.add(R.id.Linear,Frag,"fuel");
+        tr.add(R.id.Linear2,Frag2,"socre");
+        tr.commit();
+    }
+    void setup(){
+        Frag = new Group_fragment();
+        Frag2 = new Group_fragment2();
     }
 }
